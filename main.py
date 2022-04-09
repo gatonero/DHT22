53e35af92c093527abedfc2a23094795c71ec581
Christoph Schwaeppe <christoph.schwaeppe@gmail.com>
2022-04-09 13:17:20 +0200

    minmax 1.0

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ oled = OLED()
 oled.switchOn()
 oled.clearAll()
 
+max = -100
+min = 100
+
 while True:
     
     try:
@@ -22,21 +25,27 @@ while True:
         temp = sensor.temperature()
         hum = sensor.humidity()
         temp_f = temp * (9/5) + 32.0
-        #lcd.clear()
+
+        if temp < min:
+            min = temp
+        if temp > max:
+            max = temp
+
         message1 = ('TempC   : %3.1f C' % temp)
         message2 = ('TempF   : %3.1f F' %temp_f)
         message3 = ('Feuchte : %3.1f %%' %hum)
-        message4 = ('================')
+        #message4 = ('Min: %3.1f %%   Max: %3.1f %% ' %min %max)
+        message4 = ('Mi:%3.1f ' %min + 'Mx: %3.1f' %max)
         
         lcd.print(message1, 0)
         lcd.print(message2, 1)
-        lcd.print(message3, 2)
-        lcd.print(message4, 3)
+        #lcd.print(message3, 2)
+        lcd.print(message4, 2)
 
         oled.writeAt(message1, 0, 0)
         oled.writeAt(message2, 0, 1)
-        oled.writeAt(message3, 0, 2)
-        oled.writeAt(message4, 0, 3)
+        #oled.writeAt(message3, 0, 2)
+        oled.writeAt(message4, 0, 2)
 
     except OSError as e:
         print('Failed to read sensor.')
