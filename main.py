f4b9ab4ecab2399df59c0a5073e2ef437fb6ea3a
Christoph Schwaeppe <christoph.schwaeppe@gmail.com>
2022-04-10 15:42:46 +0200

    Korrektur Anzeige OLED

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from oled import OLED
 from parallaxlcd import CharLCD
 
 sensor = dht.DHT22(Pin(14))
-lcd = CharLCD()
-lcd.setup()
+#lcd = CharLCD()
 
 oled = OLED()
 oled.switchOn()
@@ -37,14 +36,14 @@ while True:
         #message4 = ('Min: %3.1f %%   Max: %3.1f %% ' %min %max)
         message4 = ('Mi:%3.1f ' %min + 'Mx: %3.1f' %max)
         
-        lcd.print(message1, 0)
-        lcd.print(message2, 1)
-        #lcd.print(message3, 2)
-        lcd.print(message4, 2)
+        # lcd.print(message1, 0)
+        # lcd.print(message2, 1)
+        # #lcd.print(message3, 2)
+        # lcd.print(message4, 2)
 
         oled.writeAt(message1, 0, 0)
-        oled.writeAt(message2, 0, 1)
-        #oled.writeAt(message3, 0, 2)
+        #oled.writeAt(message2, 0, 1)
+        oled.writeAt(message3, 0, 1)
         oled.writeAt(message4, 0, 2)
 
     except OSError as e:
